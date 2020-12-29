0fe86d279bd4ca38a9f9748da24de726943394c4
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-12-29 08:26:19 -0600

    Sprite Impoter
    Your welcome Ivy

---
+++ Assets/Scripts/Editor/TexturePostProsseser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TexturePostProsseser : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
+
+        if (isInSpritesDirectory)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            TextureImporterSettings textureImporterSettings = new TextureImporterSettings();
+            textureImporter.ReadTextureSettings(textureImporterSettings);
+            textureImporterSettings.spriteMeshType = SpriteMeshType.FullRect;
+            textureImporter.SetTextureSettings(textureImporterSettings);
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.filterMode = FilterMode.Point;
+            textureImporter.spritePixelsPerUnit = texture.width;
+            texture.Apply();
+        }
+    }
+}
