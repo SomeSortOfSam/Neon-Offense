fcba4d48f514f6721c78e388aa6e972b8ab26e7e
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-12-29 11:45:54 -0600

    Rebounder

---
+++ Assets/Scripts/Gamplay/GamplayCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class GamplayCamera : MonoBehaviour
+{
+    public static GamplayCamera instance;
+    new public Camera camera;
+    public Bounds CameraBounds
+    {
+        get
+        {
+            float height = camera.orthographicSize * 2;
+            float width = camera.aspect * height;
+            return new Bounds((Vector2)transform.position, new Vector2(width, height));
+        }
+        set
+        {
+            camera.orthographicSize = BoundsToOrithigraphicSize(value);
+            transform.position = new Vector3(value.center.x, value.center.y, -10);
+        }
+    }
+    private float BoundsToOrithigraphicSize(Bounds value)
+    {
+        float orthigraphicSize = value.extents.y;
+        if (value.size.x > value.size.y)
+        {
+            orthigraphicSize = value.extents.x / camera.aspect;
+        }
+        return orthigraphicSize;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+}
---
+++ Assets/Scripts/Gamplay/Rebounder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rebounder : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Bounds cameraBounds = GamplayCamera.instance.CameraBounds;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Abs(transform.position.x) > cameraBounds.extents.x ? -transform.position.x : transform.position.x;
+        newPos.y = Mathf.Abs(transform.position.y) > cameraBounds.extents.y ? -transform.position.y : transform.position.y;
+        transform.position = newPos == (Vector2)transform.position ? transform.position : (Vector3)newPos;
+    }
+}
