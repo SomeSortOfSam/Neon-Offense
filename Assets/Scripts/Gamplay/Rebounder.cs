791c2b17f0e057a5a2818df48f209192a4ddacd9
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-12-29 14:04:39 -0600

    UnBox

--- Assets/Scripts/Gamplay/Rebounder.cs
+++ Assets/Scripts/Gamplay/Rebounder.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
 public class Rebounder : MonoBehaviour
 {
-    new public BoxCollider2D collider;
-    public virtual void Start()
-    {
-        collider = GetComponent<BoxCollider2D>();
-    }
+    public Bounds bounds;
     // Update is called once per frame
     protected virtual void FixedUpdate()
     {
         Bounds cameraBounds = GamplayCamera.instance.CameraBounds;
         Vector2 newPos = new Vector2();
-        newPos.x = Mathf.Abs(transform.position.x + collider.size.x/2f) > cameraBounds.extents.x ? -transform.position.x : transform.position.x;
-        newPos.y = Mathf.Abs(transform.position.y + collider.size.y/2f) > cameraBounds.extents.y ? -transform.position.y : transform.position.y;
+        newPos.x = Mathf.Abs(transform.position.x + bounds.extents.x) > cameraBounds.extents.x ? -transform.position.x : transform.position.x;
+        newPos.y = Mathf.Abs(transform.position.y + bounds.extents.y) > cameraBounds.extents.y ? -transform.position.y : transform.position.y;
         transform.position = newPos == (Vector2)transform.position ? transform.position : (Vector3)newPos;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, bounds.size);
     }
 }
