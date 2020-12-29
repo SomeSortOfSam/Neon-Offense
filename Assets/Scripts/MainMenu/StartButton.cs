450ea47308943d2dad3f5e7fb72b4807ce402294
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-12-29 08:50:33 -0600

    StartButton

---
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class StartButton : Button
+    {
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            base.OnPointerClick(eventData);
+            
+        }
+    }
+}
+
