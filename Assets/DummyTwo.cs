5c76495643ab7d6fd44092e74a27a4ebc0fb873e
TheNinjamaster <1theninjamaster@gmail.com>
2020-12-29 15:11:26 -0600

    Created EnemyManager
    EnemyManager gets all the gameobjects attached to the DummyScript script and puts them in a list, removing them once they are dead and outputting a message when they're all dead

---
+++ Assets/DummyTwo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyTwo : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("Die");
+    }
+
+    public IEnumerator Die()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(this.gameObject);
+    }
+}
