6c028b5fbb59119c993f6114524b52aa35d87ddf
TheNinjamaster <1theninjamaster@gmail.com>
2020-12-29 13:11:49 -0600

    Shooting
    Created Bullet and Gun classes for future weapons to inherit + made BasicPlayerGun and BasicPlayerBullet for shooting.

---
+++ Assets/Scripts/Gamplay/weapons/BasicPlayerBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerBullet : Bullet
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        this.friendly = true;
+        rb.velocity = new Vector2(0, this.speed);
+        StartCoroutine("TimeToDeath");
+    }
+
+    public IEnumerator TimeToDeath()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Gamplay/weapons/Gun.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public int ammo;
+    public int firingPause;
+}
