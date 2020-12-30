53a025d11f896bf3941ea109dad112ea02552bfc
Portal <57550537+Theportalmasters@users.noreply.github.com>
2020-12-30 14:44:06 -0600

    Game Win and Lose

--- Assets/Scripts/Gamplay/StateManager.cs
+++ Assets/Scripts/Gamplay/StateManager.cs
@@ -1,24 +1,53 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StateManager : MonoBehaviour
 {
     public static StateManager instance;
+    public static bool lost = false;
     public GameObject winObject;
+    public CanvasGroup loseImage;
     public Vector3 winObjectSpawnPos;
+
+    public static void Lose(int health)
+    {
+        if(health < 0)
+        {
+            Lose();
+        }
+    }
     public static void Lose()
     {
-        throw new NotImplementedException();
+        lost = true;
+        instance.StartCoroutine("LoseCorutine");
+    }
+
+    private System.Collections.IEnumerator LoseCorutine()
+    {
+        float time = 0;
+        while(time < 1)
+        {
+            loseImage.alpha = time;
+            time += .01f;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
     public static void Win()
     {
-        Instantiate(instance.winObject, instance.winObjectSpawnPos, Quaternion.identity);
+        if(!lost)
+        {
+            instance.winObject.SetActive(true);
+        }
     }
 
     private void Start()
     {
-        instance = this;    
+        instance = this;
+        lost = false;
     }
 
     private void OnDrawGizmosSelected()
