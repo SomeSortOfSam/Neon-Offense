7c8ae03325d37ef1d2764b57b75332a3bd3e1086
TheNinjamaster <1theninjamaster@gmail.com>
2020-12-31 02:33:57 -0600

    Dragoon commit
    got some of the dragoon done.

---
+++ Assets/Scripts/Enemy Scripts/DragoonEnd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragoonEnd : Enemy
+{
+    public List<GameObject> links;
+
+    public int delay;
+
+    void Update()
+    {
+        foreach (GameObject g in links)
+        {
+            if (g == null)
+            {
+                links.Remove(g);
+            }
+        }
+        if (links[0].Equals(this.gameObject))
+        {
+            //move on own
+        }
+        else
+        {
+            //follow the leader
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/DragoonLink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragoonLink : Enemy
+{
+
+    public List<GameObject> links;
+
+    public int delay;
+    
+    void Update()
+    {
+        foreach (GameObject g in links)
+        {
+            if(g == null)
+            {
+                links.Remove(g);
+            }
+        }
+        if (links[0].Equals(this.gameObject))
+        {
+            //move on own
+        }
+        else
+        {
+            //follow the leader
+        }
+
+        //shoot sometimes
+    }
+}
